01855f1c49f4f1b7221590a8758a7f87e978d4a5
mclc1120664311 <1120664311@qq.com>
2022-05-06 12:15:40 +0800

    feat: webpack 配置调整

--- webpack/webpack.confg.js
+++ webpack/webpack.confg.js
@@ -1,36 +1,33 @@
-const { argv } = require("./utils");
 const { merge } = require("webpack-merge");
 const path = require("path");
 const devConfig = require("./webpack.base.config");
 const prodConfig = require("./webpack.prod.base.config");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { mode, env } = argv;
-const page = env.split("=")[1];
-let baseConfig = mode === "production" ? prodConfig : devConfig;
+module.exports = ({ platform }, { mode }) => {
+  let baseConfig = mode === "production" ? prodConfig : devConfig;
+  let HtmlPluginConfig;
+  let entryPath = "";
+  if (platform === "h5") {
+    entryPath = "../src/pages/H5/src/index.tsx";
+    HtmlPluginConfig = {
+      template: path.resolve("./public/template/index-h5.html"),
+      favicon: path.resolve("./public/favicon.ico"),
+    };
+  } else {
+    entryPath = "../src/pages/PC/src/index.tsx";
+    HtmlPluginConfig = {
+      template: path.resolve("./public/template/index.html"),
+      favicon: path.resolve("./public/favicon.ico"),
+    };
+  }
 
-let HtmlPluginConfig;
-let entryPath = "";
-if (page === "h5") {
-  entryPath = "../src/pages/H5/src/index.tsx";
-  HtmlPluginConfig = {
-    template: path.resolve("./public/template/index-h5.html"),
-    favicon: path.resolve("./public/favicon.ico"),
-  };
-} else {
-  entryPath = "../src/pages/PC/src/index.tsx";
-  HtmlPluginConfig = {
-    template: path.resolve("./public/template/index.html"),
-    favicon: path.resolve("./public/favicon.ico"),
-  };
-}
-
-baseConfig.plugins.push(new HtmlWebpackPlugin(HtmlPluginConfig));
-
-module.exports = merge(baseConfig, {
-  entry: path.resolve(__dirname, entryPath),
-  output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "[name].[contenthash].js",
-  },
-});
+  baseConfig.plugins.push(new HtmlWebpackPlugin(HtmlPluginConfig));
+  return merge(baseConfig, {
+    entry: path.resolve(__dirname, entryPath),
+    output: {
+      path: path.resolve(__dirname, "../dist"),
+      filename: "[name].[contenthash].js",
+    },
+  });
+};
