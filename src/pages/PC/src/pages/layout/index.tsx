b820c1575a060daed8b8c6ac53b20585aa84fd01
mclc1120664311 <1120664311@qq.com>
2022-05-09 10:31:08 +0800

    feat: 路由配置修改

--- src/pages/PC/src/pages/layout/index.tsx
+++ src/pages/PC/src/pages/layout/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./index.less";
-import { HashRouter, Switch, Route } from "react-router-dom";
-import Home from "../Home";
-import Detail from "../detail";
+import { HashRouter, Switch } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+import routesConfig from "../../routeConfig";
 import Navigate from "../../components/Navgate/index";
 import BannerComponent from "@/pages/PC/src/components/BannerComponent";
 import Loading from "../../components/LoadingWindows";
@@ -39,14 +39,7 @@ function App() {
             boxContent={"AFK Pools"}
           ></BannerComponent>
           <HashRouter>
-            <Switch>
-              <Route exact path="/">
-                <Home></Home>
-              </Route>
-              <Route path="/detail">
-                <Detail></Detail>
-              </Route>
-            </Switch>
+            <Switch>{renderRoutes(routesConfig)}</Switch>
           </HashRouter>
         </div>
       </div>
