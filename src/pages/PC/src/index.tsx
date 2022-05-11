b10f173a7d3b88b0cb43797af0e7e879962ffd17
mclc1120664311 <1120664311@qq.com>
2022-05-11 12:04:40 +0800

    feat: north.config.js 结构修改

--- src/pages/PC/src/index.tsx
+++ src/pages/PC/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import './css/font.css';
 import "./css/global.css";
 import App from './pages/layout';
-import north from "../../../../webpack/north.config.js";
+import north from "@config/north.config.js";
 import { North } from '@zblock/north';
 
 if (process.env.NODE_ENV === "production") {
